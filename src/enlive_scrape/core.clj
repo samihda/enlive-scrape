d09d524ebc81f3767f3484d9f79f945affb337cd
samihda <samihda@users.noreply.github.com>
2018-02-26 00:16:33 +0100

    Scratch

---
+++ src/enlive_scrape/core.clj
@@ -0,0 +1,14 @@
+(ns enlive-scrape.core
+  (:require [net.cgrand.enlive-html :as html]))
+
+(def url "https://news.ycombinator.com/")
+
+(defn fetch-url [url]
+  (html/html-resource (java.net.URL. url)))
+
+(defn get-titles [nodes]
+  (map html/text (html/select nodes [:td.title :a])))
+
+(defn -main []
+  (doseq [line (get-titles (fetch-url url))]
+    (println line)))
